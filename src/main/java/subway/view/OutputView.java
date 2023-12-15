ad64107a15673fd64a6c4bbf7382b14659d2a5b8
yoonjuho <dbswn990@naver.com>
2023-12-15 18:49:05 +0900

    feat(OutputView): 입력에 대한 결과를 출력한다.

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,5 +1,7 @@
 package subway.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public void printBegin(){
@@ -9,4 +11,36 @@ public class OutputView {
         System.out.println("## 원하는 기능을 선택하세요.");
     }
 
+    public void printRouteStandard(){
+        System.out.println("## 경로 기준\n" +
+                "1. 최단 거리\n" +
+                "2. 최소 시간\n" +
+                "B. 돌아가기" + System.lineSeparator());
+        System.out.println("## 원하는 기능을 선택하세요.");
+    }
+
+    public void printStartStation(){
+        System.out.println(System.lineSeparator() + "## 출발역을 입력하세요.");
+    }
+
+    public void printEndStation(){
+        System.out.println(System.lineSeparator() + "## 도착역을 입력하세요.");
+    }
+
+    public void printResult(int distance, int time, List<String> stations){
+        System.out.println(System.lineSeparator() +
+                "## 조회 결과\n" +
+                "[INFO] ---\n" +
+                String.format("[INFO] 총 거리: %dkm\n", distance) +
+                String.format("[INFO] 총 소요 시간: %d분\n", time) +
+                "[INFO] ---\n");
+        for(String station : stations){
+            System.out.println(String.format("[INFO] %s\n",station));
+        }
+        System.out.println();
+    }
+
+    public void printException(String message) {
+        System.out.println("[ERROR] " + message);
+    }
 }
