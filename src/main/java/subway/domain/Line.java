7ca1e8807d70c7ff6f11d5258301d23b683b90d4
yoonjuho <dbswn990@naver.com>
2023-12-15 13:35:21 +0900

    feat(Line): 노선에 해당하는 역들을 등록한다.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,14 +1,24 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
 
+    private List<Station> stations;
+
     public Line(String name) {
         this.name = name;
+        this.stations = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public void registerStation(List<Station> stations){
+        this.stations = new ArrayList<>(stations);
     }
 
     // 추가 기능 구현
