766496379812bb0cf45c4e7b2409d735d39e2400
yoonjuho <dbswn990@naver.com>
2023-12-15 18:22:44 +0900

    feat(OutputView): 메인 화면을 출력한다.

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,12 @@
+package subway.view;
+
+public class OutputView {
+
+    public void printBegin(){
+        System.out.println("## 메인 화면\n" +
+                "1. 경로 조회\n" +
+                "Q. 종료" + System.lineSeparator());
+        System.out.println("## 원하는 기능을 선택하세요.");
+    }
+
+}
