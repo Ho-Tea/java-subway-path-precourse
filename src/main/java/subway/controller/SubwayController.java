d9cd56e300423500391457d44c4dc11a2ca609c1
yoonjuho <dbswn990@naver.com>
2023-12-15 18:23:09 +0900

    feat(SubwayController): 원하는 기능을 입력받는다.

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -1,11 +1,20 @@
 package subway.controller;
 
 import subway.domain.*;
+import subway.view.OutputView;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class SubwayController {
+    private final Scanner scanner;
+    private final OutputView outputView;
 
+    public SubwayController(Scanner scanner, OutputView outputView) {
+        this.scanner = scanner;
+        this.outputView = outputView;
+        init();
+    }
 
     private void init(){
         for(StationName stationName : StationName.values()){
@@ -19,12 +28,33 @@ public class SubwayController {
     }
 
     public void run(){
-        init();
-        final Scanner scanner = new Scanner(System.in);
+        while (inputMainFunction()){
+            outputView.
+        }
+
         String start = scanner.nextLine();
         String end = scanner.nextLine();
         Calculator calculator = new Calculator(new Station(StationName.valueOf(start)), new Station(StationName.valueOf(end)));
+        List<String> stations =  calculator.findShortest();
+        for(String station : stations){
+            System.out.println(station);
+        }
+        System.out.println(calculator.sumDistance(stations));
+        System.out.println(calculator.sumTime(stations));
     }
+
+    public boolean inputMainFunction(){
+        outputView.printBegin();
+        String choose = scanner.nextLine();
+        if(choose.equals("Q")){
+            return false;
+        }
+        if(Integer.parseInt(choose) == 1){
+            return true;
+        }
+        throw new IllegalArgumentException("해당 문자에 일치하는 기능이 없습니다.");
+    }
+
 
 
 }
