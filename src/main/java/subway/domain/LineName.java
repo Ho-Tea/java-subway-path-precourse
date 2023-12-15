611154635d1a81a85f1554d8130e491b3682080e
yoonjuho <dbswn990@naver.com>
2023-12-15 13:18:39 +0900

    feat(LineName): 노선당 지하철 역 이름 추가

--- src/main/java/subway/domain/LineName.java
+++ src/main/java/subway/domain/LineName.java
@@ -1,14 +1,18 @@
 package subway.domain;
 
-public enum LineName {
-    LINE_2("2호선"),
-    LINE_3("3호선"),
+import java.util.List;
 
-    LINE_SHINBUNDANG("신분당선");
+public enum LineName {
+    LINE_2("2호선", List.of(StationName.교대역, StationName.강남역, StationName.역삼역)),
+    LINE_3("3호선", List.of(StationName.교대역, StationName.남부터미널역, StationName.양재역, StationName.매봉역)),
+
+    LINE_SHINBUNDANG("신분당선", List.of(StationName.강남역, StationName.양재역, StationName.양재시민의숲역));
 
     private String name;
+    private List<StationName> stationNames;
 
-    LineName(String name) {
+    LineName(String name, List<StationName> stationNames) {
         this.name = name;
+        this.stationNames = stationNames;
     }
 }
