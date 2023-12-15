6892618709a36b90ee59498fffdc477df93c5314
yoonjuho <dbswn990@naver.com>
2023-12-15 13:35:45 +0900

    feat(LineName): 역의 이름으로 역을 생성해 반환한다.

--- src/main/java/subway/domain/LineName.java
+++ src/main/java/subway/domain/LineName.java
@@ -1,6 +1,8 @@
 package subway.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public enum LineName {
     LINE_2("2호선", List.of(StationName.교대역, StationName.강남역, StationName.역삼역)),
@@ -14,5 +16,19 @@ public enum LineName {
     LineName(String name, List<StationName> stationNames) {
         this.name = name;
         this.stationNames = stationNames;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<StationName> getStationNames() {
+        return stationNames;
+    }
+
+    public static List<Station> stations(LineName lineName){
+        return lineName.stationNames.stream()
+                .map(stationName -> new Station(stationName.name()))
+                .collect(Collectors.toList());
     }
 }
